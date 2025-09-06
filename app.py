7553ffb53f235f327bdc00c009bd75ce7ebcfa1f
Willian Vantier <williansv45@gmail.com>
2025-09-06 09:28:17 -0300

    Implementação do Sistema de loogin.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from database import db
+from flask import Flask, redirect, request, render_template, url_for
+from flask_login import login_required, login_user, logout_user
+from database import db, Usuario
 from login import lm
 
 app = Flask(__name__)
@@ -11,13 +12,47 @@ db.init_app(app)
 lm.init_app(app)
 
 
+@lm.user_loader
+def user_loader(id):
+    return Usuario.query.get(int(id))
 
-@app.route('/')
+
+@app.route('/', methods = ['GET','POST'])
 def login():
-    return 'login'
+    
+    if request.method == 'POST':
+        user = request.form['userForm']
+        senha = request.form['senhaForm']
+        
+        busca = Usuario.query.filter_by(user = user, senha = senha).first()
+        
+        if busca:
+            login_user(busca)
+            return redirect(url_for('home'))
+        else:
+            return redirect(url_for('login'))
+            
+    
+    return render_template('login.html')
 
+
+@app.route('/home')
+@login_required
+def home():
+    return 'Logado com sucesso'
+
+@app.route('/logout')
+@login_required
+def logout():
+    user_loader()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        if not Usuario.query.filter_by(user = 'admin').first():
+            admin = Usuario(user = 'admin', senha = '12345', tipo = 'admin')
+            db.session.add(admin)
+            db.session.commit()
+            
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,12 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
-import enum
-
-class TipoEnum(enum.Enum):
-    ADMIN = "admin"
-    FUNCIONARIO = "funcionario"
-    COZINHA = "cozinha"
-
 
 db = SQLAlchemy()
 
@@ -14,4 +7,4 @@ class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     user = db.Column(db.String(), nullable = False, unique = True)
     senha = db.Column(db.String(), nullable = False)
-    tipo = db.Column(db.Enum(TipoEnum), nullable=False, default=TipoEnum.FUNCIONARIO)
+    tipo = db.Column(db.Enum('cozinha','funcionario','admin'), nullable=False, default='funcionario')
